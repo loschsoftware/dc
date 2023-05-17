d24b81968924e8f9bf8d3c821fa97a7ed78985d6
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-17 16:11:34 +0200

    Add core library project

---
+++ src/LoschScript.Core/stdout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoschScript.Core;
+
+public static class stdout
+{
+    public static void println(string msg) => Console.WriteLine(msg);
+    public static void println(int msg) => Console.WriteLine(msg);
+    public static void println(double msg) => Console.WriteLine(msg);
+    public static void println(float msg) => Console.WriteLine(msg);
+    public static void println(char msg) => Console.WriteLine(msg);
+    public static void println(bool msg) => Console.WriteLine(msg);
+    public static void println(object msg) => Console.WriteLine(msg);
+
+    public static void print(string msg) => Console.Write(msg);
+    public static void print(int msg) => Console.Write(msg);
+    public static void print(double msg) => Console.Write(msg);
+    public static void print(float msg) => Console.Write(msg);
+    public static void print(char msg) => Console.Write(msg);
+    public static void print(bool msg) => Console.Write(msg);
+    public static void print(object msg) => Console.Write(msg);
+}
