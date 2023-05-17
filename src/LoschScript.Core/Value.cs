7bf0c565ddbe70cc6413faa8c03dfdf0f1d25a0d
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-17 22:49:19 +0200

    Add refvalue and reftype methods

--- src/LoschScript.Core/Value.cs
+++ src/LoschScript.Core/Value.cs
@@ -1,4 +1,6 @@
-﻿namespace LoschScript.Core;
+﻿using System;
+
+namespace LoschScript.Core;
 
 public static class Value
 {
@@ -7,4 +9,8 @@ public static class Value
     public static void ignore(object obj) { }
 
     public static void discard(object obj) { }
+
+    public static Type reftype(TypedReference tr) => __reftype(tr);
+
+    public static object refvalue(TypedReference tr) => __refvalue(tr, object);
 }
