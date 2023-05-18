d5ffce7bff6d05a8870e7d10860cb34f460b5f55
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-18 16:05:23 +0200

    Add entry point attribute

---
+++ src/LoschScript.Core/EntryPointAttribute.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace LoschScript.Core;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+public sealed class EntryPointAttribute : Attribute { }
