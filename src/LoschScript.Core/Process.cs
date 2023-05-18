07f76cf49907d9e00540e293ab6d3db469c7a543
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-18 17:09:35 +0200

    Add exit functions

---
+++ src/LoschScript.Core/Process.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LoschScript.Core;
+
+/// <summary>
+/// Provides functionality for interacting with the current process.
+/// </summary>
+public static class Process
+{
+    /// <summary>
+    /// Exits the current process with the specified exit code.
+    /// </summary>
+    /// <param name="exitCode">The exit code.</param>
+    public static void exit(int exitCode) => Environment.Exit(exitCode);
+
+    /// <summary>
+    /// Exits the current process with exit code 0.
+    /// </summary>
+    public static void exit() => Environment.Exit(0);
+}
