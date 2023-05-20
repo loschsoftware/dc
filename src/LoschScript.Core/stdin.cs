d59a11a42b9fb412760dd3ac0d5219c27c492b5e
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-20 22:25:31 +0200

    Add standard functions for reading from the console and parsing

--- src/LoschScript.Core/stdin.cs
+++ src/LoschScript.Core/stdin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace LoschScript.Core;
 
@@ -11,7 +12,199 @@ public static class stdin
     /// Reads a line from the standard input.
     /// </summary>
     /// <returns>Returns a line of text read from the standard input.</returns>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
     public static string scan() => Console.ReadLine();
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static int rdint() => int.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an unsigned integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static uint rduint() => uint.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static int rdint32() => int.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an unsigned integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static uint rduint32() => uint.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a 64-bit integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static long rdint64() => long.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an unsigned 64-bit integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static ulong rduint64() => ulong.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a 16-bit integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static short rdint16() => short.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an unsigned 16-bit integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static ushort rduint16() => ushort.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an 8-bit integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static sbyte rdint8() => sbyte.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to an unsigned 8-bit integer.
+    /// </summary>
+    /// <returns>The converted integer.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static byte rduint8() => byte.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a boolean.
+    /// </summary>
+    /// <returns>The converted boolean.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static bool rdbool() => bool.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a 32-bit floating point number.
+    /// </summary>
+    /// <returns>The converted floating point number.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static float rdfloat32() => float.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a 64-bit floating point number.
+    /// </summary>
+    /// <returns>The converted floating point number.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static double rdfloat64() => double.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a decimal number.
+    /// </summary>
+    /// <returns>The converted decimal number.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static decimal rddec() => decimal.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input and attempts to convert it to a Unicode character.
+    /// </summary>
+    /// <returns>The converted character.</returns>
+    /// <exception cref="FormatException"/>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="OverflowException"/>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static char rdchar() => char.Parse(Console.ReadLine());
+
+    /// <summary>
+    /// Reads a line from the standard input.
+    /// </summary>
+    /// <returns>The read line.</returns>
+    /// <exception cref="IOException"/>
+    /// <exception cref="OutOfMemoryException"/>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    public static string rdstr() => Console.ReadLine();
 
     /// <summary>
     /// Reads a line from the standard input with a prompt.
