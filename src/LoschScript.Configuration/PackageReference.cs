cc625fa3f4f72a216928862990b9d71ef2b05e37
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-21 18:32:01 +0200

    Add NuGet package references to lsconfig

---
+++ src/LoschScript.Configuration/PackageReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Serialization;
+namespace Losch.LoschScript.Configuration;
+
+[Serializable]
+[XmlRoot("PackageReference")]
+public class PackageReference : Reference
+{
+    [XmlAttribute("Version")]
+    public string Version { get; set; }
+
+    [XmlText]
+    public string PackageId { get; set; }
+}
