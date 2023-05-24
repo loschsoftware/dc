d4181da798bc95b1a5728096f2864b9abc78b6d1
Jonas Glaser-Heiß <loschag@outlook.de>
2023-05-24 18:18:36 +0200

    Use single quotes for consistency

--- src/LoschScript/CLI/Helpers.cs
+++ src/LoschScript/CLI/Helpers.cs
@@ -229,7 +229,7 @@ internal static class Helpers
                 row,
                 col,
                 LS0009_TypeNotFound,
-                $"The name \"{name}\" could not be resolved.");
+                $"The name '{name}' could not be resolved.");
         }
 
         return type;
