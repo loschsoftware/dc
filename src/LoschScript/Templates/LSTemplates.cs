ed5c2a3d54716c14a7a7405d4e5c78517bec924e
Jonas <loschag@outlook.de>
2023-10-07 23:21:33 +0200

    Update project templates

--- src/LoschScript/Templates/LSTemplates.cs
+++ src/LoschScript/Templates/LSTemplates.cs
@@ -1,6 +1,6 @@
 ﻿using Losch.LoschScript.Configuration;
-using LoschScript.Configuration;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml.Serialization;
 
@@ -26,9 +26,9 @@ Console.WriteLine ""Hello World!""";
     public static readonly string Library =
 @"import System
 
-type Library =
-    Main =
-        Console.WriteLine ""Hello World!""";
+type Library = {
+    .
+}";
 
     /// <summary>
     /// Contains a template for a class file.
@@ -37,15 +37,16 @@ type Library =
 @"import System
 export $$NAMESPACE$$
 
-type Class1 =
-    .";
+type Class1 = {
+    .
+}";
 
     /// <summary>
     /// Contains a template for an embedded script file.
     /// </summary>
     public static readonly string EmbeddedScript =
 @"@script
-@v48
+@v481
 
 WriteLine ""Hello World!""";
 
@@ -56,7 +57,7 @@ WriteLine ""Hello World!""";
 @"import LoschScript.Scripting
 
 ScriptHost.Run ""script.els""";
-    
+
     internal static int CreateStructure(string[] args)
     {
         if (args.Length < 3)
@@ -66,63 +67,51 @@ ScriptHost.Run ""script.els""";
         }
 
         string sourceDir = Path.Combine(Directory.GetCurrentDirectory(), args[2]);
+        Directory.CreateDirectory(sourceDir);
 
-        if (!Directory.Exists(sourceDir))
-            Directory.CreateDirectory(sourceDir);
+        FileStream configFile = File.Create(Path.Combine(sourceDir, "lsconfig.xml"));
+
+        XmlSerializerNamespaces ns = new();
+        ns.Add("", "");
+
+        XmlSerializer xmls = new(typeof(LSConfig));
+
+        LSConfig config = new()
+        {
+            AssemblyName = args[2],
+            DefaultNamespace = args[2][0].ToString().ToUpper() + string.Join("", args[2].Skip(1)),
+            VersionInfo = new()
+            {
+                Version = "1.0.0.0"
+            },
+            BuildOutputDirectory = ".\\build"
+        };
 
         switch (args[1])
         {
             case "console":
-                var fs = File.Create(Path.Combine(sourceDir, "app.ls"));
-                byte[] buffer = Encoding.UTF8.GetBytes(LSTemplates.Console.ToCharArray());
-                fs.Write(buffer, 0, LSTemplates.Console.Length);
-
-                var configFile = File.Create(Path.Combine(sourceDir, "lsconfig.xml"));
-
-                XmlSerializerNamespaces ns = new();
-                ns.Add("", "");
-
-                var xmls = new XmlSerializer(typeof(LSConfig));
-
-                xmls.Serialize(configFile, new LSConfig()
                 {
-                    ApplicationType = ApplicationType.Console,
-                    AssemblyName = "app",
-                    DefaultNamespace = "Application",
-                    VersionInfo = new()
-                    {
-                        Version = "1.0.0.0"
-                    },
-                    BuildOutputDirectory = ".\\build"
-                }, ns);
-                break;
+                    var fs = File.Create(Path.Combine(sourceDir, "main.ls"));
+                    fs.Write(Encoding.UTF8.GetBytes(Console.ToCharArray()), 0, Console.Length);
+
+                    config.ApplicationType = ApplicationType.Console;
+                    xmls.Serialize(configFile, config, ns);
+
+                    break;
+                }
+
             case "script":
-                var _fs = File.Create(Path.Combine(sourceDir, "host.ls"));
-                byte[] _buffer = Encoding.UTF8.GetBytes(LSTemplates.EmbeddedScriptHost.ToCharArray());
-                _fs.Write(_buffer, 0, LSTemplates.EmbeddedScriptHost.Length);
-
-                var __fs = File.Create(Path.Combine(sourceDir, "script.els"));
-                byte[] __buffer = Encoding.UTF8.GetBytes(LSTemplates.EmbeddedScript.ToCharArray());
-                __fs.Write(__buffer, 0, LSTemplates.EmbeddedScript.Length);
-
-                var _configFile = File.Create(Path.Combine(sourceDir, "lsconfig.xml"));
-
-                XmlSerializerNamespaces _ns = new();
-                _ns.Add("", "");
-
-                var _xmls = new XmlSerializer(typeof(LSConfig));
-                _xmls.Serialize(_configFile, new LSConfig()
                 {
-                    ApplicationType = ApplicationType.Console,
-                    AssemblyName = "app",
-                    DefaultNamespace = "Application",
-                    VersionInfo = new()
-                    {
-                        Version = "1.0.0.0"
-                    },
-                    BuildOutputDirectory = ".\\build"
-                }, _ns);
-                break;
+                    FileStream fs = File.Create(Path.Combine(sourceDir, "host.ls"));
+                    fs.Write(Encoding.UTF8.GetBytes(EmbeddedScriptHost.ToCharArray()), 0, EmbeddedScriptHost.Length);
+
+                    FileStream _fs = File.Create(Path.Combine(sourceDir, "script.els"));
+                    _fs.Write(Encoding.UTF8.GetBytes(EmbeddedScript.ToCharArray()), 0, EmbeddedScript.Length);
+
+                    config.ApplicationType = ApplicationType.Console;
+                    xmls.Serialize(configFile, config, ns);
+                    break;
+                }
         }
 
         System.Console.WriteLine($"Built new project in {sourceDir} based on template '{args[1]}'.");
