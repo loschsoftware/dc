3aab1f8dee90e5b2cea925da26eff374e6695703
Jonas <loschag@outlook.de>
2023-10-13 17:32:22 +0200

    Add attribute for global functions

---
+++ src/LoschScript.Core/Core/ClrGlobalFunctionAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+/// <summary>
+/// Declares a function to be a CLR global function contained in the special <c>&lt;Module&gt;</c> type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+public sealed class ClrGlobalFunctionAttribute : Attribute { }
