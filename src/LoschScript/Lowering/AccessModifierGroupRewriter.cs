484b4f16540eaf0a9ffb4ca531ff65e9db0934d0
Jonas <loschag@outlook.de>
2023-10-15 19:44:59 +0200

    Move extension methods to own file

--- src/LoschScript/Lowering/AccessModifierGroupRewriter.cs
+++ src/LoschScript/Lowering/AccessModifierGroupRewriter.cs
@@ -6,18 +6,6 @@ using System.Linq;
 
 namespace LoschScript.Lowering;
 
-internal static class Extensions
-{
-    public static string ReplaceFirst(this string str, string search, string replace)
-    {
-        int pos = str.IndexOf(search);
-        if (pos < 0)
-            return str;
-
-        return str[..pos] + replace + str[(pos + search.Length)..];
-    }
-}
-
 internal class AccessModifierGroupRewriter : IRewriter
 {
     public string Rewrite(IParseTree tree, LoweringListener listener)
