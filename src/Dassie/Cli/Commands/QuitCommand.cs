348dedf75619413fff2199815ac67c8be77d7196
Jonas <loschag@outlook.de>
2024-11-06 20:23:16 +0100

    Also check for missing pid.txt file

--- src/Dassie/Cli/Commands/QuitCommand.cs
+++ src/Dassie/Cli/Commands/QuitCommand.cs
@@ -21,6 +21,12 @@ internal class QuitCommand : ICompilerCommand
         Process[] processes = Process.GetProcesses();
 
         string pidFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Dassie", "pid.txt");
+        if (!File.Exists(pidFilePath))
+        {
+            LogOut.WriteLine("No file watchers running.");
+            return 0;
+        }
+
         File.ReadAllLines(pidFilePath).Select(int.Parse).ToList().ForEach(i =>
         {
             if (processes.Any(p => p.Id == i))
