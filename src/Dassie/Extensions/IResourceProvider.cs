0dc851ff4691f09a1c7838f87fdd6603fd23db01
Jonas <loschag@outlook.de>
2024-12-18 19:12:29 +0100

    Add infrastructure necessary for localization

---
+++ src/Dassie/Extensions/IResourceProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Dassie.Extensions;
+
+/// <summary>
+/// Defines a mechanism to define and override compiler resources, such as localization strings.
+/// </summary>
+public interface IResourceProvider<TRes>
+{
+    /// <summary>
+    /// The resources defined by the resource provider, stored as a dictionary with string keys.
+    /// </summary>
+    public Dictionary<string, TRes> Resources { get; }
+}
---
+++ src/Dassie/Resources/DefaultStrings.cs
@@ -0,0 +1,15 @@
+﻿using Dassie.Extensions;
+using System.Collections.Generic;
+
+namespace Dassie.Resources;
+
+internal class DefaultStrings : IResourceProvider<string>
+{
+    private static DefaultStrings _instance;
+    public static DefaultStrings Instance => _instance ??= new();
+
+    public Dictionary<string, string> Resources => new()
+    {
+        // TODO: Fill this out at some point
+    };
+}
