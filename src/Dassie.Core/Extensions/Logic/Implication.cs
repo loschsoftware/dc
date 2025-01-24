00954af029b6e29dd10f1148e12951bd69799e3c
Jonas <loschag@outlook.de>
2025-01-24 22:21:58 +0100

    Add an optional implication operator -->

---
+++ src/Dassie.Core/Extensions/Logic/Implication.cs
@@ -0,0 +1,21 @@
+﻿using Dassie.Runtime;
+
+#pragma warning disable IDE1006
+
+namespace Dassie.Extensions.Logic;
+
+/// <summary>
+/// Implements the 'implies' boolean operator (<c>--></c>).
+/// </summary>
+[ContainsCustomOperators]
+public static class Implication
+{
+    /// <summary>
+    /// Implements the 'implies' boolean operator (<c>--></c>).
+    /// </summary>
+    /// <param name="p">The proposition of the implication.</param>
+    /// <param name="q">The consequence of the implication.</param>
+    /// <returns>If <paramref name="p"/>, returns <paramref name="q"/>. Otherwise, returns <see langword="true"/>.</returns>
+    [Operator]
+    public static bool op_MinusMinusGreater(bool p, bool q) => !p || q;
+}
