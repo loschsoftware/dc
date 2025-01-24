a69ef304133d687d650888b68e7dca2d32935363
Jonas <loschag@outlook.de>
2025-01-24 22:29:09 +0100

    Change syntax of implication to ==>

--- src/Dassie.Core/Extensions/Logic/Implication.cs
+++ src/Dassie.Core/Extensions/Logic/Implication.cs
@@ -5,17 +5,17 @@
 namespace Dassie.Extensions.Logic;
 
 /// <summary>
-/// Implements the 'implies' boolean operator (<c>--></c>).
+/// Implements the 'implies' boolean operator (<c>==></c>).
 /// </summary>
 [ContainsCustomOperators]
 public static class Implication
 {
     /// <summary>
-    /// Implements the 'implies' boolean operator (<c>--></c>).
+    /// Implements the 'implies' boolean operator (<c>==></c>).
     /// </summary>
     /// <param name="p">The proposition of the implication.</param>
     /// <param name="q">The consequence of the implication.</param>
     /// <returns>If <paramref name="p"/>, returns <paramref name="q"/>. Otherwise, returns <see langword="true"/>.</returns>
     [Operator]
-    public static bool op_MinusMinusGreater(bool p, bool q) => !p || q;
+    public static bool op_EqualEqualGreater(bool p, bool q) => !p || q;
 }
