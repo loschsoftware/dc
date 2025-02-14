5b8e8ea3a34a3d011fcc81ba6d4a102898e8d6d8
Jonas <loschag@outlook.de>
2025-02-14 18:51:30 +0100

    Add string processors for en-/decoding base64

---
+++ src/Dassie.Core/Core/base64.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+#pragma warning disable IDE1006
+
+namespace Dassie.Core;
+
+/// <summary>
+/// Provides a string processor for encoding string literals as Base64.
+/// </summary>
+public class base64 : IStringProcessor<string>
+{
+    /// <inheritdoc/>
+    [Pure]
+    public static string Process(string input)
+        => Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
+}
+
+/// <summary>
+/// Provides a string processor for decoding Base64 string literals.
+/// </summary>
+public class from_base64 : IStringProcessor<string>
+{
+    /// <inheritdoc/>
+    [Pure]
+    public static string Process(string input)
+        => Encoding.UTF8.GetString(Convert.FromBase64String(input));
+}
--- src/Dassie.Core/Core/utf8.cs
+++ src/Dassie.Core/Core/utf8.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 #pragma warning disable IDE1006
 
